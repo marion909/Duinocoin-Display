57fb7f2c2968add8d4a440ebd6a2a1d32fdb9158
marion909 <103169397+marion909@users.noreply.github.com>
2024-01-14 15:27:23 +0100

    small fixes

--- duco_display.ino
+++ duco_display.ino
@@ -13,6 +13,8 @@ double lastTotalHashrate = -1.0;
 int lastDifficulty = -1;
 double lastMaxAccepted = -1.0;
 
+bool dataLoaded = false;
+
 void setup() {
   Serial.begin(115200);
 
@@ -27,17 +29,24 @@ void setup() {
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
     Serial.print(".");
-    ipsdisp.fillScreen(TFT_ORANGE);
-    displayMessage("Kein WLAN!", 10);
+    ipsdisp.fillScreen(TFT_WHITE);
+    ipsdisp.pushImage(40, 0, 240, 240, myGraphic);
+    displayMessage("Verbinde WLAN...", 190, TFT_BLACK);
   }
 
   ipsdisp.fillScreen(TFT_WHITE);
   ipsdisp.pushImage(40, 0, 240, 240, myGraphic);
+  displayMessage("Daten werden geladen...", 190, TFT_BLACK);
   delay(5000);
   ipsdisp.fillScreen(TFT_ORANGE);
+
+  dataLoaded = true;
 }
 
 void loop() {
+
+  if (dataLoaded) {
+
   if (WiFi.status() == WL_CONNECTED) {
     WiFiClientSecure secured_client;
     secured_client.setInsecure();
@@ -72,12 +81,6 @@ if (https.begin(secured_client, serverUrl)) {
           double maxAccepted = 0.0;
           int minerCount = 0;
 
-          if (minerCount < DUCO_MINER) {
-            int diff = DUCO_MINER - minerCount;
-            String message = "Achtung! " + String(diff) + " Miner sind offline.";
-
-          }
-
           JsonArray miners = doc["result"]["miners"];
           for (JsonObject miner : miners) {
             double hashrate = miner["hashrate"];
@@ -111,17 +114,23 @@ if (https.begin(secured_client, serverUrl)) {
 
         } else {
           Serial.printf("[HTTPS] GET... failed, error: %s\n", https.errorToString(httpCode).c_str());
-          displayMessage("Kein Benutzer oder HTTP-Fehler!", 10);
+          displayMessage("Kein Benutzer oder HTTP-Fehler!", 10, TFT_WHITE);
         }
 
         https.end();
       } else {
         Serial.printf("[HTTPS] Verbindung fehlgeschlagen\n");
-        displayMessage("Kein Internet!", 10);
+        displayMessage("Kein Internet!", 10, TFT_WHITE);
       }
     }
   }
-
+ dataLoaded = true;
+  } else {
+    // Display a loading message or animation while waiting for data
+    ipsdisp.fillScreen(TFT_ORANGE);
+    displayMessage("Daten werden geladen...", 10, TFT_WHITE);
+    // Add any additional loading UI logic here if needed
+  }
   Serial.println();
   Serial.println("Warte 10 Sekunden vor der nächsten Runde...");
   delay(10000);
@@ -129,17 +138,28 @@ if (https.begin(secured_client, serverUrl)) {
 
 void displayInfo(double balance, int minerCount, double totalHashrate, int difficulty, double maxAccepted) {
   ipsdisp.fillScreen(TFT_ORANGE);
-  displayMessage("Benutzer: " + String(DUCO_USERNAME), 10);
-  displayMessage("Guthaben: " + String(balance) + " Duco", 40);
-  displayMessage("Miner: " + String(minerCount), 70);
-  displayMessage("Hashrate: " + String(totalHashrate) + " kH/s", 100);
-  displayMessage("Schwierigkeit: " + String(difficulty), 130);
-  displayMessage("Max. Akzeptiert: " + String(maxAccepted), 160);
-  displayMessage("IP: " + WiFi.localIP().toString(), 190);
+  displayMessage("Benutzer: " + String(DUCO_USERNAME), 10, TFT_WHITE);
+  displayMessage("Guthaben: " + String(balance) + " Duco", 40, TFT_WHITE);
+  if (minerCount < DUCO_MINER) {
+            int diff = DUCO_MINER - minerCount;
+            String message = String(diff) + " Miner sind offline.";
+            displayMessage("Miner: " + String(minerCount), 70, TFT_RED);
+            displayMessage("Achtung! ", 100, TFT_RED);
+            displayMessage(String(message), 130, TFT_RED);
+          }else{
+  displayMessage("Miner: " + String(minerCount), 70, TFT_WHITE);
+  displayMessage("Hashrate: " + String(totalHashrate) + " kH/s", 100, TFT_WHITE);
+  displayMessage("Schwierigkeit: " + String(difficulty), 130, TFT_WHITE);
+  displayMessage("Max. Akzeptiert: " + String(maxAccepted), 160, TFT_WHITE);
+          }
+  
+
+  displayMessage("IP: " + WiFi.localIP().toString(), 190, TFT_WHITE);
 }
 
-void displayMessage(const String &message, int yPos) {
+void displayMessage(const String &message, int yPos, uint16_t textColor) {
   ipsdisp.setCursor(10, yPos, 4);
-  ipsdisp.setTextColor(TFT_WHITE);
+  ipsdisp.setTextColor(textColor);
   ipsdisp.println(message);
 }
+
